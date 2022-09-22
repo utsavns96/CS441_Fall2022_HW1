8a608fc30de3839a2ca695dea2981ba974f77f83
utsavsharma <utsavns96@gmail.com>
2022-09-21 19:48:47 -0500

    Implemented Func 4 + added logging + added dynamic output dir name for QoL

--- LogFileGenerator/src/main/scala/DistributionCSV.scala
+++ LogFileGenerator/src/main/scala/DistributionCSV.scala
@@ -1,3 +1,4 @@
+import HelperUtils.CreateLogger
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.conf.*
@@ -6,14 +7,19 @@ import org.apache.hadoop.util.*
 import org.apache.hadoop.mapred.*
 
 import java.io.IOException
+import java.text.SimpleDateFormat
 import java.util
+import java.util.Calendar
 import java.util.regex.Pattern
 import scala.jdk.CollectionConverters.*
-
-object MapReduceProgram:
+//This is the code for functionality 3
+object DistributionCSV:
+  val logger = CreateLogger(classOf[DistributionCSV.type])
   //Loading the configuration files so that we can fetch the regex that have been set in the .conf file
+  logger.info(s"Starting DistributionCSV - loading configs")
   val config: Config = ConfigFactory.load("application.conf")
   val funcconfig = config.getConfig("randomLogGenerator.functionalityconfigs.mapreducetocsv")
+
   class Map extends MapReduceBase with Mapper[LongWritable, Text, Text, IntWritable]:
     //creating a variable one to use with the mapper.
     private final val one = new IntWritable(1)
@@ -22,7 +28,6 @@ object MapReduceProgram:
 
     @throws[IOException]
     def map(key: LongWritable, value: Text, output: OutputCollector[Text, IntWritable], reporter: Reporter): Unit =
-
       //First we take the regex patterns that we want to match from the application.conf file where they have been defined.
       //Then we use matcher() to match the input value against the regex pattern.
       val injectedpattern = Pattern.compile(config.getString("randomLogGenerator.Pattern")).matcher(value.toString)
@@ -33,8 +38,9 @@ object MapReduceProgram:
       //If we find a string that satisfies the injected regex and is of the format we need (INFO/WARN/DEBUG/ERROR), we proceed to add it to our map output.
       if(injectedpattern.find() && userdefinedpattern.find())
       {
+        logger.info(s"Found a string satisfying our regex constrains")
         //val loglevel = userdefinedpattern.group()
-        //value.toString.substring(0,5) gives us the timestamp to the minute of the log message, and group() method gives us the input subsequence matched by the above matcher()
+        //value.toString.substring(0,5) gives us the timestamp to the minute of the log message, and group() method gives us the input subsequence matched by the above matcher().
         word.set(value.toString.substring(0,5) + ":00 " + userdefinedpattern.group())
         output.collect(word, one)
       }
@@ -43,13 +49,15 @@ object MapReduceProgram:
     override def reduce(key: Text, values: util.Iterator[IntWritable], output: OutputCollector[Text, IntWritable], reporter: Reporter): Unit =
       //This reducer simply adds up all the values from the map.
       val sum = values.asScala.foldLeft(0)(_+_.get)
+      logger.info(s"The sum for time interval and log message type= "+ sum)
       output.collect(key, new IntWritable(sum))
 
   @main def runMapRed(inputPath: String, outputPath: String) =
+    logger.info(s"Starting the main implementation runMapRed for DistributionCSV")
     require(!inputPath.isEmpty && !outputPath.isEmpty)
     println(inputPath)
     val conf: JobConf = new JobConf(this.getClass)
-    conf.setJobName("WordCount")
+    conf.setJobName("DistributionCSV")
     conf.set("fs.defaultFS", "local")
     conf.set("mapreduce.job.maps", "1")
     conf.set("mapreduce.job.reduces", "1")
@@ -62,5 +70,9 @@ object MapReduceProgram:
     conf.setOutputFormat(classOf[TextOutputFormat[Text, IntWritable]])
     conf.set("mapred.textoutputformat.separatorText", ",")
     FileInputFormat.setInputPaths(conf, new Path(inputPath))
-    FileOutputFormat.setOutputPath(conf, new Path(outputPath))
+    //Creating a new time format to append to our output directory
+    var timeformat = new SimpleDateFormat("dd-MM-yyyy-hh-mm")
+    //Saves the trouble of having to delete the output directory again and again
+    FileOutputFormat.setOutputPath(conf, new Path(outputPath + funcconfig.getString("OutputPath") + "_" + timeformat.format(Calendar.getInstance().getTime)))
+    logger.info(s"Job configurations set. Starting job." + conf.getJobName)
     JobClient.runJob(conf)
---
+++ LogFileGenerator/src/main/scala/LongestString.scala
@@ -0,0 +1,82 @@
+import HelperUtils.CreateLogger
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+object DescendingOrder {
+  import com.typesafe.config.{Config, ConfigFactory}
+  import org.apache.hadoop.fs.Path
+  import org.apache.hadoop.conf.*
+  import org.apache.hadoop.io.*
+  import org.apache.hadoop.util.*
+  import org.apache.hadoop.mapred.*
+
+  import java.io.IOException
+  import java.util
+  import java.util.regex.Pattern
+  import scala.jdk.CollectionConverters.*
+  //This is the implementation for Functionality 4
+  object LongestString:
+    val logger = CreateLogger(classOf[DistributionCSV.type])
+    logger.info(s"Starting LongestString - loading configs")
+    //Loading the configuration files so that we can fetch the regex that have been set in the .conf file
+    val config: Config = ConfigFactory.load("application.conf")
+    val funcconfig = config.getConfig("randomLogGenerator.functionalityconfigs.longeststring")
+    class Map extends MapReduceBase with Mapper[LongWritable, Text, Text, IntWritable]:
+      //creating the val word to hold our key
+      private val word = new Text()
+
+      @throws[IOException]
+      def map(key: LongWritable, value: Text, output: OutputCollector[Text, IntWritable], reporter: Reporter): Unit =
+
+        //First we take the regex patterns that we want to match from the application.conf file where they have been defined.
+        //Then we use matcher() to match the input value against the regex pattern.
+        val injectedpattern = Pattern.compile(config.getString("randomLogGenerator.Pattern")).matcher(value.toString)
+        val userdefinedpattern = Pattern.compile(funcconfig.getString("FindOccurrenceOf")).matcher(value.toString)
+        //val timeinterval = value.toString.substring(0,5)
+        //val matchinjected = injectedpattern.matcher(value.toString)
+        //val matcheduserdefined = userdefinedpattern.matcher(value.toString)
+        //If we find a string that satisfies the injected regex and is of the format we need (INFO/WARN/DEBUG/ERROR), we proceed to add it to our map output.
+        if(injectedpattern.find() && userdefinedpattern.find())
+        {
+          logger.info(s"Found a string satisfying our regex constrains")
+          //Setting the key as the the log message type
+          word.set(userdefinedpattern.group())
+          //Adding to our output the key and the length of the pattern that we found from the matcher() method
+          output.collect(word, new IntWritable(injectedpattern.group().length))
+        }
+
+    class Reduce extends MapReduceBase with Reducer[Text, IntWritable, Text, IntWritable]:
+      override def reduce(key: Text, values: util.Iterator[IntWritable], output: OutputCollector[Text, IntWritable], reporter: Reporter): Unit =
+        //This reducer finds the maximum value from the output of our map.
+        val longest = values.asScala.max
+        logger.info(s"The longest string for= "+ key +" is of length "+ longest)
+        output.collect(key, longest)
+
+    @main def runLongestString(inputPath: String, outputPath: String) =
+      logger.info(s"Starting the main implementation runLongestString for LongestString")
+      require(!inputPath.isEmpty && !outputPath.isEmpty)
+      println(inputPath)
+      //setting job parameters
+      val conf: JobConf = new JobConf(this.getClass)
+      conf.setJobName("LongestString")
+      conf.set("fs.defaultFS", "local")
+      conf.set("mapreduce.job.maps", "1")
+      conf.set("mapreduce.job.reduces", "1")
+      conf.setOutputKeyClass(classOf[Text])
+      conf.setOutputValueClass(classOf[IntWritable])
+      conf.setMapperClass(classOf[Map])
+      conf.setCombinerClass(classOf[Reduce])
+      conf.setReducerClass(classOf[Reduce])
+      conf.setInputFormat(classOf[TextInputFormat])
+      conf.setOutputFormat(classOf[TextOutputFormat[Text, IntWritable]])
+      conf.set("mapred.textoutputformat.separatorText", ",")
+      FileInputFormat.setInputPaths(conf, new Path(inputPath))
+      //Creating a new time format to append to our output directory
+      var timeformat = new SimpleDateFormat("dd-MM-yyyy-hh-mm")
+      //Saves the trouble of having to delete the output directory again and again
+      FileOutputFormat.setOutputPath(conf, new Path(outputPath + funcconfig.getString("OutputPath")+"_"+timeformat.format(Calendar.getInstance().getTime)))
+      logger.info(s"Job configurations set. Starting job." + conf.getJobName)
+      JobClient.runJob(conf)
+
+}
