7f3484d1e41613039d71c4e47cfa150adf00a8d6
utsavsharma <utsavns96@gmail.com>
2022-10-08 00:01:39 -0500

    Adding more test cases and updating Readme to V1.2

--- LogFileGenerator/src/test/scala/TestCases.scala
+++ LogFileGenerator/src/test/scala/TestCases.scala
@@ -3,8 +3,10 @@ import org.scalatest.funsuite.AnyFunSuite
 
 import java.util.regex.Pattern
 import com.typesafe.config.{Config, ConfigFactory}
+
 import scala.runtime.stdLibPatches.Predef.assert
 import java.io.File
+import java.text.SimpleDateFormat
 
 object TestCases
   class TestCase extends AnyFunSuite:
@@ -41,7 +43,7 @@ object TestCases
       val config: Config = ConfigFactory.load("application.conf")
       val funcconfig = config.getConfig("randomLogGenerator.functionalityconfigs.longeststring")
       val value="14:35:50.652 [scala-execution-context-global-21] ERROR HelperUtils.Parameters$ - P#~\"PoX@Oc+f!&Q4h3TM:ioE(+B(\"\"`*3U2y;2~[hQL1Js{Iez<(A&CP"
-      val userdefinedpattern = Pattern.compile(funcconfig.getString("FindOccurrenceOf")).matcher(value.toString)
+      val userdefinedpattern = Pattern.compile(funcconfig.getString("FindOccurrenceOf")).matcher(value)
       assert(userdefinedpattern.find())
     }
 
@@ -50,7 +52,7 @@ object TestCases
       val config: Config = ConfigFactory.load("application.conf")
       val funcconfig = config.getConfig("randomLogGenerator.functionalityconfigs.longeststring")
       val value = "@org.apache.hadoop.metrics2.annotation.Metric(sampleName=Ops, always=false, valueName=Time, about=, interval=10, type=DEFAULT, value=[])"
-      val userdefinedpattern = Pattern.compile(funcconfig.getString("FindOccurrenceOf")).matcher(value.toString)
+      val userdefinedpattern = Pattern.compile(funcconfig.getString("FindOccurrenceOf")).matcher(value)
       assert(!userdefinedpattern.find())
     }
 
@@ -58,7 +60,7 @@ object TestCases
       //check if we are matching our injected pattern correctly. It should not match in this case.
       val config: Config = ConfigFactory.load("application.conf")
       val value = "14:35:50.652 [scala-execution-context-global-21] ERROR HelperUtils.Parameters$ - P#~\"PoX@Oc+f!&Q4h3TM:ioE(+B(\"\"`*3U2y;2~[hQL1Js{Iez<(A&CP"
-      val injectedpattern = Pattern.compile(config.getString("randomLogGenerator.Pattern")).matcher(value.toString)
+      val injectedpattern = Pattern.compile(config.getString("randomLogGenerator.Pattern")).matcher(value)
       assert(!injectedpattern.find())
     }
 
@@ -66,8 +68,34 @@ object TestCases
       //check if we are matching our injected pattern correctly. It should match in this case.
       val config: Config = ConfigFactory.load("application.conf")
       val value = "14:35:49.958 [scala-execution-context-global-21] INFO  HelperUtils.Parameters$ - hxgQ_i:JDGT7hN7wbg3ae0cg0ag2NG-xk\\Bcb."
-      val injectedpattern = Pattern.compile(config.getString("randomLogGenerator.Pattern")).matcher(value.toString)
+      val injectedpattern = Pattern.compile(config.getString("randomLogGenerator.Pattern")).matcher(value)
       assert(injectedpattern.find())
     }
-
-
+    test("Unit Test for Log string starting with timestamp regex positive") {
+      //check if the log string is correctly identified as starting with a timestamp
+      val value = "14:35:49.958 [scala-execution-context-global-21] INFO  HelperUtils.Parameters$ - hxgQ_i:JDGT7hN7wbg3ae0cg0ag2NG-xk\\Bcb."
+      val timestamp = Pattern.compile("^(\\d\\d:\\d\\d)").matcher(value.substring(0,5))
+      assert(timestamp.find())
+    }
+    test("Unit Test for Log string starting with timestamp regex negative") {
+      //check if the log string is correctly identified as not starting with a timestamp
+      val value = "org.apache.commons.configuration2.ex.ConfigurationException: Could not locate: org.apache.commons.configuration2.io.FileLocator@173ed316[fileName=hadoop-metrics2-jobtracker.properties,basePath=<null>,sourceURL=,encoding=<null>,fileSystem=<null>,locationStrategy=<null>]"
+      val timestamp = Pattern.compile("^(\\d\\d:\\d\\d)").matcher(value.substring(0, 5))
+      assert(!timestamp.find())
+    }
+    test("Unit Test for timestamp between time window positive") {
+      //check if we we are identifying our time window and timestamps correctly
+      val config: Config = ConfigFactory.load("application.conf")
+      val funcconfig = config.getConfig("randomLogGenerator.functionalityconfigs.mapreducetocsv")
+      val value = "14:37:28"
+      val timeformat: SimpleDateFormat = new SimpleDateFormat("hh:mm:ss")
+      assert((timeformat.parse(value.substring(0,8)).after(timeformat.parse(funcconfig.getString("StartTime"))) && timeformat.parse(value.substring(0,8)).before(timeformat.parse(funcconfig.getString("EndTime")))))
+    }
+    test("Unit Test for timestamp between time window negative") {
+      //check if we we are identifying our time window and timestamps correctly
+      val config: Config = ConfigFactory.load("application.conf")
+      val funcconfig = config.getConfig("randomLogGenerator.functionalityconfigs.mapreducetocsv")
+      val value = "12:07:55"
+      val timeformat: SimpleDateFormat = new SimpleDateFormat("hh:mm:ss")
+      assert(!(timeformat.parse(value.substring(0, 8)).after(timeformat.parse(funcconfig.getString("StartTime"))) && timeformat.parse(value.substring(0, 8)).before(timeformat.parse(funcconfig.getString("EndTime")))))
+    }
